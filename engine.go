479dbf5234a74699e7cc521fa1f4163e16879848
stevenzack <zigzigcheers@gmail.com>
2023-11-23 04:19:59 -0500

    remove-gin

--- engine.go
+++ engine.go
@@ -3,34 +3,24 @@ package fn
 import (
 	"context"
 	"log"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/gin-gonic/gin"
 )
 
-type Engine struct {
-	*gin.Engine
-}
-
-func Default() *Engine {
-	return &Engine{
-		Engine: gin.Default(),
-	}
-}
-
-func (s *Engine) Run(addr ...string) error {
+func Run(serveMux http.Handler) error {
 	lambda.Start(func(ctx context.Context, r request) (*response, error) {
 		// convert to gin.Context
 		req, e := r.ToHttpRequest()
 		if e != nil {
 			log.Println(e)
-			return jsonResponse(500, gin.H{
+			return jsonResponse(500, map[string]any{
 				"message": e.Error(),
 			})
 		}
 		w := newResponse()
 
-		s.ServeHTTP(w, req)
+		serveMux.ServeHTTP(w, req)
 		w.Wrap()
 		return w, nil
 	})
